2f3f284137c17e1ec61ce801eb2b7713b3c8f216
Kunal Deshmane <kunaldeshmane18@gmail.com>
2022-09-13 23:00:11 +0530

    Error removed from first1.c file.

--- First1.c
+++ First1.c
@@ -1,14 +1,10 @@
 #include<stdio.h>
-
 extern int no;  // Declaration of variable
-
 int main()
 {
     printf("%d\n",no);
-
     return 0;
 }
-
 // gcc  First.c  Second.c   -o   Myexe
 // ./Myexe      For Linux or MACOS
 // Myexe        For windows
